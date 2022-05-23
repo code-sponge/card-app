81693e1dd2b86b3b35eb7dea30ce73b33db015d0
code sponge <spon9ec0de@gmail.com>
2022-05-23 22:25:21 +0900

    Add login component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import { auth } from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import './index.module.css';
+import AuthService from './service/auth_service';
 
+const authService = new AuthService();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService} />
   </React.StrictMode>
 );
