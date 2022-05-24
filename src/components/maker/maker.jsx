488a018fb26199a6d64b6bb2af3107b0acb776da
code sponge <spon9ec0de@gmail.com>
2022-05-24 17:39:07 +0900

    Add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
@@ -7,6 +7,41 @@ import Header from '../header/header';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ashley',
+      company: 'Ohouse',
+      theme: 'colorful',
+      job: 'Software Engineer',
+      email: 'spon9ec0de@gmail.com',
+      message: 'I am proud of myself',
+      fileName: 'ashley',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Dana',
+      company: 'Beyond Style',
+      theme: 'dark',
+      job: 'Hair Designer',
+      email: 'dana.yeo@gmail.com',
+      message: 'I love bbodori',
+      fileName: 'dana',
+      fileURL: 'dana.png',
+    },
+    {
+      id: '3',
+      name: 'Podo',
+      company: 'Bbodoll House',
+      theme: 'light',
+      job: 'Interior Designer',
+      email: 'bbodori04@naver.com',
+      message: 'I love podo',
+      fileName: 'podo',
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor className={styles.editor} />
-        <Preview className={styles.preview} />
+        <Editor className={styles.editor} cards={cards} />
+        <Preview className={styles.preview} cards={cards} />
       </div>
       <Footer />
     </section>
