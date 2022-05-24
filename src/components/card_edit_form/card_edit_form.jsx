6aba98b6af1cbe5de994ee283eca02df30b368d8
code sponge <spon9ec0de@gmail.com>
2022-05-24 22:38:35 +0900

    Handle state change on editor

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,31 +1,71 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card }) => {
-  const { name, company, job, email, message, theme, fileName, fileURL } = card;
-  const onSubmit = () => {};
+const CardEditForm = ({ card, onDelete, onUpdate }) => {
+  const { name, company, job, email, message, theme, fileName, fileURL, id } =
+    card;
+
+  const onChange = (e) => {
+    if (e.currentTarget == null) {
+      return;
+    }
+    e.preventDefault();
+
+    onUpdate({
+      ...card,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+  const onSubmit = (e) => {
+    onDelete(card);
+  };
   return (
     <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" value={name} />
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
       <input
         className={styles.input}
         type="text"
         name="company"
         value={company}
+        onChange={onChange}
       />
-      <select className={styles.select} name="theme" value={theme}>
+      <select
+        className={styles.select}
+        name="theme"
+        value={theme}
+        onChange={onChange}
+      >
         <option value="light">Light</option>
         <option value="dark">Dark</option>
         <option value="colorful">Colorful</option>
       </select>
-      <input className={styles.input} type="text" name="job" value={job} />
-      <input className={styles.input} type="text" name="email" value={email} />
+      <input
+        className={styles.input}
+        type="text"
+        name="job"
+        value={job}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
       <textarea
         className={styles.textarea}
         name="message"
         value={message}
+        onChange={onChange}
       ></textarea>
       <div className={styles.fileInput}>
         <ImageFileInput />
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Header from '../header/header';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: '1',
       name: 'Ashley',
       company: 'Ohouse',
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: 'ashley',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'Dana',
       company: 'Beyond Style',
@@ -30,7 +30,7 @@ const Maker = ({ authService }) => {
       fileName: 'dana',
       fileURL: 'dana.png',
     },
-    {
+    3: {
       id: '3',
       name: 'Podo',
       company: 'Bbodoll House',
@@ -41,7 +41,7 @@ const Maker = ({ authService }) => {
       fileName: 'podo',
       fileURL: null,
     },
-  ]);
+  });
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -55,16 +55,32 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor className={styles.editor} cards={cards} addCard={addCard} />
+        <Editor
+          className={styles.editor}
+          cards={cards}
+          deleteCard={deleteCard}
+          createOrUpdateCard={createOrUpdateCard}
+        />
         <Preview className={styles.preview} cards={cards} />
       </div>
       <Footer />
